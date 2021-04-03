a7668c77a8ae3786a2c217e739325258ee16af27
Trevor Zou <tzou@olin.edu>
2021-04-02 23:46:06 -0400

    finish graphs for computational essay

--- functions.py
+++ functions.py
@@ -9,6 +9,14 @@ import itertools
 import math
 from math import isnan
 import numpy as np
+from pylab import plot, title, xlabel, ylabel, savefig, legend, array
+
+def get_image(url):
+    response = requests.get(url)
+    file = open("class21.png", "wb")
+    file.write(response.content)
+    file.close()
+    return file
 
 def get_data():
     """
@@ -114,7 +122,7 @@ def engineer(nasa_astronaut_dataset):
             continue
         non +=1
     total = engineer + non
-    print(f"{engineer / total * 100} %of Astronauts majored in some kind of Engineering")
+    print(f"{engineer / total * 100} % of Astronauts majored in some kind of Engineering")
 
 
 def gender_military(nasa_astronaut_dataset):
@@ -124,8 +132,7 @@ def gender_military(nasa_astronaut_dataset):
     gender_military = gender_occurrence["Military Rank"]
     gender_occurrence_name = gender_occurrence["Name"]
     gender = ["Female", "Male"]
-    
-    print(gender_occurrence)
+
     count_row = nasa_astronaut_dataset.shape[0]
     plt.style.use("ggplot")
     plt.bar(gender, gender_occurrence_name, width=0.8, label='Civilian', color='silver')
@@ -235,7 +242,8 @@ def female_astronauts_decade(nasa_astronaut_dataset):
         gender_frequency = frequency(decade, 6)
         values = list(gender_frequency.values())
         if len(values) == 2:
-            frequency_per_decade.append(values[1])
+            percentage_women = values[1] / sum(values) * 100
+            frequency_per_decade.append(percentage_women)
         else:
             frequency_per_decade.append(0)
         start += 10
@@ -245,25 +253,43 @@ def female_astronauts_decade(nasa_astronaut_dataset):
     decade = [1950, 1960, 1970, 1980, 1990, 2000]
     plt.step(decade, frequency_per_decade, color = 'blue')
     plt.xlabel('Decade')
-    plt.ylabel('Number of Female Astronauts Selected')
+    plt.ylabel('% Female Astronauts (pct.)')
     plt.title('Female Astronauts Selected Per Decade')
 
     plt.show()
 
 def military_college_over_time(nasa_astronaut_dataset):
     values = []
-    for i in range(19):
-        dict = frequency(nasa_astronaut_dataset, 7)
-        military = 0
-        not_gradschool = 0
+    for i in range(23):
+        grouper = nasa_astronaut_dataset[nasa_astronaut_dataset.Group == i]
+        
+        college_list = []
+        colleges = grouper["Alma Mater"]
+        for i in colleges:
+            i = str(i)
+            split_list = i.split("; ")
+            college_list += split_list
+            
+        military_prep = 0
+        non = 0
         for i in college_list:
-            if type(i) == str:
-                gradschool +=1
-                continue
-            not_gradschool += 1
+            if "US " not in i and "The Citadel" not in i:
+                military_prep += 1
+            else:
+                non += 1
+        if (military_prep + non) > 0:
+            values.append(military_prep / (military_prep + non)* 100)
+            continue
+        values.append(0)
+    year = range(23)
+    plt.scatter(year, values)
+    plt.ylim(5,110)
+    plt.xlabel('Group Number')
+    plt.ylabel('% From a Military College(pct.)')
+    plt.title('Percentage of Astronauts from a Military College')
 
-        return [gradschool, not_gradschool]
-    
+    plt.show()
+
 
 def grad_school_over_time(nasa_astronaut_dataset):
     
@@ -277,7 +303,7 @@ def grad_school_over_time(nasa_astronaut_dataset):
     intheeight = grad_school_vs_not_grad_school(eighties)
     
     ninties = filter_by_year(nasa_astronaut_dataset, 1990, 2014)
-    intehnine = grad_school_vs_not_grad_school(ninties)
+    inthenine = grad_school_vs_not_grad_school(ninties)
     
     labels ='Gradschool','Not'
     
@@ -288,14 +314,16 @@ def grad_school_over_time(nasa_astronaut_dataset):
     axs[0, 1].set_title('1970s')
     axs[1, 0].pie(intheeight, labels=labels, autopct='%1.1f%%')
     axs[1, 0].set_title('1980s')
-    axs[1, 1].pie(intehnine, labels=labels, autopct='%1.1f%%')
+    axs[1, 1].pie(inthenine, labels=labels, autopct='%1.1f%%')
     axs[1, 1].set_title('90s and 2000s')
     
-    fig.suptitle('Astronaut Post-Grad Trends through the Decades', fontsize=16)
+    fig.suptitle('Astronaut Post-Grad Trends through the Decades',x=.5,y=1, fontsize=13)
 
     # Hide x labels and tick labels for top plots and y ticks for right plots.
     for ax in axs.flat:
         ax.label_outer()
+    ttl = ax.title
+    ttl.set_position([.5, 1.5])
         
 def first_v_last(nasa_astronaut_dataset):
     first_three = filter_by_group(nasa_astronaut_dataset, 1, 1)
@@ -316,7 +344,7 @@ def first_v_last(nasa_astronaut_dataset):
     plt.legend(loc="upper left")
     plt.show()
     
-    last_three = filter_by_group(nasa_astronaut_dataset, 17, 17)
+    last_three = filter_by_group(nasa_astronaut_dataset, 20, 20)
     gender_occurrence = last_three.groupby('Gender').count()
     gender_military = gender_occurrence["Military Rank"]
     gender_occurrence_name = gender_occurrence["Name"]
